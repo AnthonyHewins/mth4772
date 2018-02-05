a93dae45ea7af2774fdafcbdf1c96fcf37ed17b7
AnthonyHewins <aahewins@oakland.edu>
2018-02-04 21:18:24 -0500

    Chinese remainder theorem working on nonstupid inputs

--- crt.py
+++ crt.py
@@ -1,19 +1,26 @@
 from euclidean_algorithm import euclidean_algorithm as gcd
 
 def crt(mods, congruencies):
-    # Find the M's
-
-    big_m = reduce(lambda x,y:return x*y, mods)
+    big_m = 1
+    for i in mods:
+        big_m *= i
+    
     m = []
     for i in mods:
         m += [big_m / i]
 
+    y = []
     for i in range(len(mods)):
         coefficient = m[i] % mods[i]
-        inverse = None
-        for j in range(1, mods[i]):
-            if j * coefficient % mods[i] == congruencies[i]:
-                inverse = j
+        for j in range(mods[i]):
+            if coefficient * j % mods[i] == 1:
+                y += [j]
+                break
+
+    for i in range(len(y)):
+        y[i] *= m[i] * congruencies[i]
+        
+    return sum(y) % big_m
         
 def relatively_prime(array):
     if type(array) is int: return True
@@ -29,9 +36,16 @@ def relatively_prime(array):
 if __name__ == "__main__":
     from argparse import ArgumentParser
     
+    def positive_integer(x):
+        try:
+            x = int(x)
+            return abs(x)
+        except:
+            raise TypeError("All entries need to be positive ints, or it will force that type on them.")
+            
     parser = ArgumentParser(description="Chinese remainder theorem")
-    parser.add_argument("-a", "--array-of-mods", type=int, nargs='+', help="The relatively prime numbers to use in the mod")
-    parser.add_argument("-c", "--congruencies", type=int, nargs='+', help="The congruent numbers you want to solve")
+    parser.add_argument("-a", "--array-of-mods", type=positive_integer, nargs='+', help="The relatively prime numbers to use in the mod")
+    parser.add_argument("-c", "--congruencies", type=positive_integer, nargs='+', help="The congruent numbers you want to solve")
     args = parser.parse_args()
 
     if len(args.array_of_mods) != len(args.congruencies):
@@ -39,5 +53,14 @@ if __name__ == "__main__":
         exit()
     
     if not relatively_prime(args.array_of_mods):
-        print("The mods aren't relatively prime, exiting")
-        exit()    
+        print("The mods aren't relatively prime, chinese remainder theorem cannot solve this, exiting")
+        exit()
+
+    print("Solving this system of equations:")
+    for i in range(len(args.congruencies)):
+        print("x === %d (mod %d)" % (args.congruencies[i], args.array_of_mods[i]))
+        
+    for i in range(len(args.array_of_mods)):
+        args.congruencies[i] %= args.array_of_mods[i]
+        
+    print(crt(args.array_of_mods, args.congruencies))
