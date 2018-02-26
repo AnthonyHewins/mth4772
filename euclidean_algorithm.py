81892865c4d764ac71c72f9506eda0bf727562f4
AnthonyHewins <aahewins@oakland.edu>
2018-02-26 13:35:26 -0500

    Added support for extended euclidean algorithm

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -1,32 +1,20 @@
-class Iteration():
-    def __init__(self, dividend=0, divisor=0, multiples=0, remainder=0):
-        dividend = dividend
-        divisor = divisor
-        multiples = multiples
-        remainder = remainder
+def extended_euclidean_algorithm(int1,int2):
+    if int1 < int2:
+        int1, int2 = int2, int1
 
-"""
-def linear_combination(gcd, stack):
-    previous_dividend, previous_divisor = None, None
-    if stack:
-        iteration = stack.pop()
-        previous_dividend = iteration.dividend
-        previous_divisor = iteration.divisor
+    x,y,u,v = 0,1,1,0
+    while int1 != 0:
+        quotient, remainder = int2//int1, int2%int1
+        m, n = x-u*quotient, y-v*quotient
+        int2,int1, x,y, u,v = int1,remainder, u,v,m,n
+    return x, y
 
-    while stack:
-        iteration = stack.pop()
-        dividend = iteration.dividend
-        divisor = iteration.divisor
-        remainder = iteration.remainder
-"""     
-        
-def euclidean_algorithm(int1, int2, suppress_prints=False):
+def euclidean_algorithm(int1, int2, suppress_prints=True):
     if int1 == int2:
         return int1
     if int1 < int2:
         int1, int2 = int2, int1
 
-    stack = []
     dividend = int1
     divisor = int2
     multiples = int1 // int2
@@ -35,7 +23,6 @@ def euclidean_algorithm(int1, int2, suppress_prints=False):
     while remainder != 0:
         if not suppress_prints: print("%d = %d(%d) + %d" % (dividend, divisor, multiples, remainder))
 
-        stack += [Iteration(dividend=dividend, divisor=divisor, multiples=multiples, remainder=remainder)]
         dividend = divisor
         divisor = remainder
         multiples = dividend // divisor
@@ -48,22 +35,33 @@ if __name__ =="__main__":
 
     def valid_int(x):
         try:
-            x = int(x)
+            return abs(int(x))
         except:
-            raise TypeError("Must be an int")
-        if x > 0:
-            return x
-        else:
-            raise TypeError("Not allowing negatives for this particular program")
+            raise TypeError("Euclidean algorithm only makes sense with integer inputs")
     
     parser = ArgumentParser(description="Euclidean Algorithm (add 2 numbers to the commandline)")
     parser.add_argument("int1", type=valid_int, help="One of the ints (automatically detects which is larger)")
     parser.add_argument("int2", type=valid_int, help="One of the ints (automatically detects which is larger)")
-    parser.add_argument("--no-lin-combination", action='store_true', help="Don't find a linear combination")
+    parser.add_argument('-nl', "--no-linear-combination", action='store_true', help="If you don't want to see a linear combination afterwards, flag this")
+    parser.add_argument('-s', "--suppress-prints", action='store_true', help="If you don't want the process to print the process out, flag this")
     args = parser.parse_args()
 
-    gcd = euclidean_algorithm(args.int1, args.int2)
-    print("GCD:",gcd)
-
-#    if not args.no_lin_combination:
-#       linear_combination(gcd, stack)
+    if args.int1 < args.int2: args.int1, args.int2 = args.int2, args.int1
+    
+    if not args.suppress_prints:
+        print("Euclidean algorithm:")
+        print("====================================")
+    gcd = euclidean_algorithm(args.int1, args.int2, suppress_prints=args.suppress_prints)
+    print("GCD: %d" % gcd)
+    print("\n")
+    
+    if not args.no_linear_combination:
+        x,y = extended_euclidean_algorithm(args.int1, args.int2)
+    
+    if not args.suppress_prints:    
+        print("Extended Euclidean algorithm:")
+        print("====================================")
+    print("%d = (%d)%d + (%d)%d" % (gcd, x, args.int1, y, args.int2))
+    print("All solutions:")
+    print("x = %d + %dn" % (y, args.int1 / gcd))
+    print("y = %d - %dn" % (x, args.int2 / gcd))
